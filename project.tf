7486dbbbe87c4cbaf71e271d877c18a3366a0f76
t-kikai <p.kikai0509@gmail.com>
2022-10-20 20:10:26 +0900

    update terraform setting

--- project.tf
+++ project.tf
@@ -2,7 +2,6 @@ resource "vercel_project" "with_git" {
   name = "sample-app-for-vercel"
   git_repository = {
     type = "github"
-    # ユーザー名/リポジトリ名
-    repo = "<リポジトリのパス>"
+    repo = "t-kikai/vercel-terraform"
   }
 }
